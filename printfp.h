eb8df9e851f6436f9569d0e64b6621fb5c0dc39b
JohnSamy2004 <jsamy2950@gmail.com>
2023-08-22 09:29:31 -0700

    0file

--- printfp.h
+++ printfp.h
@@ -6,15 +6,4 @@
 
 int _printf(const char *format, ...);
 
-/**
- * struct print - struct printf
- * @type: specifier char
- * @f: pointer
- */
-typedef struct print
-{
-    char type;
-    int (*f)(va_list);
-} print_t;
-
 #endif
